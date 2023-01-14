01a3ba551e9cd9464452be668f603bb2b9e2df37
Pranit Harekar <17886017+Pranit-Harekar@users.noreply.github.com>
2023-01-14 17:51:15 -0500

    Android browserPlugin

---
+++ README.md
@@ -0,0 +1,5 @@
+# MultipleWebviewsMultipleHostsThroughLAN
+
+This is a demo app that shows how two web apps rendered on separate webviews on
+separate hosts (like iOS & Android) can communicate with each other through
+local area network (LAN)
--- android/app/src/main/java/com/pranitharekar/android/MainActivity.kt
+++ android/app/src/main/java/com/pranitharekar/android/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.pranitharekar.android
 
 import android.content.Context
-import android.content.Intent
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.text.format.Formatter
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebSettings
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.pranitharekar.android.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var webview: WebView
+    private val browserPlugin = "browserPlugin"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        webview = binding.webview
+        setupWebviewWrapper()
 
         // UDP broadcast
         val udpServer = BroadcastSocketServer()
@@ -44,7 +51,11 @@ class MainActivity : AppCompatActivity() {
             httpServer.start(hostAddress, hostPort) {
                 udpServer.stop()
                 runOnUiThread {
-                    binding.textviewFirst.text = it
+                    webview.visibility = WebView.VISIBLE
+                    val js = "var bc2 = new BroadcastChannel(\"app_2\"); bc2.postMessage(\"$it\");"
+                    webview.post {
+                        webview.evaluateJavascript(js, null)
+                    }
                 }
             }
         }
@@ -56,5 +67,22 @@ class MainActivity : AppCompatActivity() {
         val wifiManager =
             this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         return Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
+    }
+
+    private inner class JavaScriptInterface {
+        @JavascriptInterface
+        fun postMessage(message: String) {
+            Log.d("JavascriptInterface", "JavaScriptInterface postMessage received: $message")
+        }
+    }
+
+    private fun setupWebviewWrapper() {
+        val webSettings: WebSettings = webview.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.databaseEnabled = true
+
+        webview.addJavascriptInterface(JavaScriptInterface(), browserPlugin)
+        webview.loadUrl("file:///android_asset/app2.html")
     }
 }
